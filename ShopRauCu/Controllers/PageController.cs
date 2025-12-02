60e9742260b67c441b5364d575fd76a93e800a68
phanduyhao <haomrvuii@gmail.com>
2025-12-03 00:31:36 +0700

    update

---
+++ ShopRauCu/Controllers/PageController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShopRauCu.Models;
+
+namespace ShopRauCu.Controllers
+{
+    public class PageController : Controller
+    {
+
+        private readonly AppDbContext _context;
+
+        public PageController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Submit(Feedback model)
+        {
+            model.CreatedAt = DateTime.Now;
+            model.UpdatedAt = DateTime.Now;
+
+            _context.Feedbacks.Add(model);
+            _context.SaveChanges();
+
+            TempData["success"] = "Cảm ơn bạn! Phản hồi của bạn đã được gửi thành công.";
+
+            return RedirectToAction("Contact");
+        }
+    }
+}
---
+++ ShopRauCu/ViewComponents/CategoryMenuViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShopRauCu.Models;
+
+public class CategoryMenuViewComponent : ViewComponent
+{
+    private readonly AppDbContext _context;
+
+    public CategoryMenuViewComponent(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var categories = await _context.Categories
+            .Where(c => c.ParentId == null)
+            .Include(c => c.InverseParent)
+            .OrderBy(c => c.Title)
+            .ToListAsync();
+
+        return View(categories);
+    }
+}
