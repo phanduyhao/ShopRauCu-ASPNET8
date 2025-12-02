c475f1a7944a645087f28a243b38baf3e0e743b0
phanduyhao <haomrvuii@gmail.com>
2025-12-03 01:29:59 +0700

    update

--- ShopRauCu/Controllers/ProductController.cs
+++ ShopRauCu/Controllers/ProductController.cs
@@ -1,12 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShopRauCu.Models;
+using X.PagedList;
 
 namespace ShopRauCu.Controllers
 {
     public class ProductController : Controller
     {
-        public IActionResult Index()
+        private readonly AppDbContext _context;
+
+        public ProductController(AppDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public IActionResult Index(int? page)
+        {
+            int pageNumber = page ?? 1; // trang hiện tại
+            int pageSize = 12;          // số sản phẩm mỗi trang
+
+            var products = _context.Products
+                                    .Include(m => m.Category)
+                                   .Where(p => p.IsActive)
+                                   .OrderByDescending(p => p.Id)
+                                   .ToPagedList(pageNumber, pageSize);
+
+            return View(products);
         }
     }
 }
